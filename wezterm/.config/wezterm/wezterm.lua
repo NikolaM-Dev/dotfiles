778a8d9fa10be8f72ceb7fedc734ab4ce533d506
Juan David Merchan Torres <juan1014166@gmail.com>
2023-06-17 12:05:54 -0500

    feat(wezterm): Add base config

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,60 @@
+local wezterm = require('wezterm')
+
+local function font(opts)
+	return wezterm.font_with_fallback({
+		opts,
+		'Symbols Nerd Font Mono',
+	})
+end
+
+local config = {
+	color_scheme = 'Catppuccin Mocha',
+
+	font = font({ family = 'JetBrainsMono NF', weight = 'Regular' }),
+	font_size = 11,
+	font_rules = {
+		{
+			italic = true,
+			intensity = 'Normal',
+			font = font({
+				family = 'JetBrainsMono NF',
+				style = 'Italic',
+			}),
+		},
+		{
+			italic = true,
+			intensity = 'Half',
+			font = font({
+				family = 'JetBrainsMono NF',
+				style = 'Italic',
+				weight = 'Medium',
+			}),
+		},
+		{
+			italic = true,
+			intensity = 'Bold',
+			font = font({
+				family = 'JetBrainsMono NF',
+				style = 'Italic',
+				weight = 'Bold',
+			}),
+		},
+	},
+
+	enable_tab_bar = false,
+	hide_tab_bar_if_only_one_tab = true,
+	tab_bar_at_bottom = false,
+	use_fancy_tab_bar = false,
+
+	cursor_blink_rate = 530,
+
+	window_background_opacity = 0.9,
+
+	window_close_confirmation = 'NeverPrompt',
+
+	warn_about_missing_glyphs = false,
+
+	term = 'wezterm',
+}
+
+return config
