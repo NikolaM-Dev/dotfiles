3e6141b2239b74cdc03069be9a1ccae750ec95be
Juan David Merchan Torres <juan1014166@gmail.com>
2024-05-26 08:29:47 -0500

    feat(wezterm): Add missing settings

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -5,51 +5,18 @@ local config = c.get_config()
 
 f.setup_font(config)
 
-	font = font({ family = base_font_family, weight = 'Regular' }),
-	font_size = 11,
-	font_rules = {
-		{
-			italic = true,
-			intensity = 'Normal',
-			font = font({
-				family = base_font_family,
-				style = 'Italic',
-			}),
-		},
-		{
-			italic = true,
-			intensity = 'Half',
-			font = font({
-				family = base_font_family,
-				style = 'Italic',
-				weight = 'Medium',
-			}),
-		},
-		{
-			italic = true,
-			intensity = 'Bold',
-			font = font({
-				family = base_font_family,
-				style = 'Italic',
-				weight = 'Bold',
-			}),
-		},
-	},
-
-	enable_tab_bar = false,
-	hide_tab_bar_if_only_one_tab = true,
-	tab_bar_at_bottom = false,
-	use_fancy_tab_bar = false,
-
-	cursor_blink_rate = 530,
-
-	window_background_opacity = 0.9,
-
-	window_close_confirmation = 'NeverPrompt',
-
-	warn_about_missing_glyphs = false,
-
-	term = 'wezterm',
-}
+config.adjust_window_size_when_changing_font_size = false
+config.audible_bell = 'Disabled'
+config.automatically_reload_config = true
+config.color_scheme = 'Catppuccin Mocha'
+config.enable_scroll_bar = false
+config.enable_tab_bar = false
+config.enable_wayland = false
+config.force_reverse_video_cursor = false
+config.freetype_load_target = 'HorizontalLcd'
+config.freetype_render_target = 'HorizontalLcd'
+config.window_background_opacity = 0.95
+config.window_close_confirmation = 'NeverPrompt'
+config.window_padding = { bottom = 8, left = 8, right = 8, top = 8 }
 
 return config
