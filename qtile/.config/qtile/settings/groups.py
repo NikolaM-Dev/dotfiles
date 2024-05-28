456d8b6086ea40c68fbd0fda82b5b7c2fe19cae0
Juan David Merchan Torres <juan1014166@gmail.com>
2024-05-27 20:37:59 -0500

    style(qtile): Change formatting

--- qtile/.config/qtile/settings/groups.py
+++ qtile/.config/qtile/settings/groups.py
@@ -1,20 +1,6 @@
-# Qtile workspaces
-
-from libqtile.config import Key, Group
 from libqtile.command import lazy
+from libqtile.config import Key, Group
 from settings.keys import mod, keys
-
-# Get the icons at https://www.nerdfonts.com/cheat-sheet (you need a Nerd Font)
-# Icons:
-# nf-fa-firefox,
-# nf-fae-python,
-# nf-dev-terminal,
-# nf-fa-code,
-# nf-seti-config,
-# nf-mdi-folder,
-# nf-mdi-image,
-# nf-fa-video_camera,
-# nf-mdi-layers
 
 groups = [
     Group(i) for i in [
