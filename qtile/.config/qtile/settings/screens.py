323e51be3aa857d71be1af93f540114daf1ec1d1
Juan David Merchan Torres <juan1014166@gmail.com>
2024-06-07 17:29:23 -0500

    feat(qtile-screens): Change opacity

--- qtile/.config/qtile/settings/screens.py
+++ qtile/.config/qtile/settings/screens.py
@@ -8,8 +8,7 @@ import subprocess
 
 
 def status_bar(widgets):
-    return bar.Bar(widgets, 24, opacity=0.92)
-
+    return bar.Bar(widgets, 24, opacity=0.95)
 
 screens = [Screen(top=status_bar(primary_widgets))]
 
