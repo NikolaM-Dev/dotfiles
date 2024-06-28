86843799bb095ed26ebb36aaa7a9eca7ae3f7260
Juan David Merchan Torres <juan1014166@gmail.com>
2024-06-28 08:15:32 -0500

    feat(qtile-autostart): Change autostart script

--- qtile/.config/qtile/autostart.sh
+++ qtile/.config/qtile/autostart.sh
@@ -1,5 +1,22 @@
 #!/bin/sh
 
+# envs
+export QT_STYLE_OVERRIDE=kvantum
+
+# Remove suspend
+xset -dpms
+xset s noblank
+xset s off
+
+# Daemons
+n-backup-second-brain &
+# rclone mount google-drive: ~/Documents/drive &
+flameshot &
+nm-applet &
+picom -b --config ~/.config/picom/picom.conf &
+sh .fehbg &
+udiskie -t &
+
 # systray battery icon
 # cbatticon -u 5 &
 # systray volume
