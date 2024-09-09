5024ae812273da7fcda806277616be5276a751a6
Juan David Merchan Torres <juan1014166@gmail.com>
2024-09-09 18:38:29 -0500

    feat(wezterm-fonts): Add jetbrains-mono-nf font config

---
+++ wezterm/.config/wezterm/fonts/jetbrains-mono-nf.lua
@@ -0,0 +1,39 @@
+local f = require('fonts')
+
+local font = f.get_font()
+
+local M = {}
+
+function M.setup_font(config)
+	local SCALE = 1
+
+	config.cell_width = 0.9
+	config.font_size = 13
+
+	config.font = font({ family = 'JetBrainsMono Nerd Font', scale = SCALE, weight = 'Regular' })
+
+	config.font_rules = {
+		{
+			font = font({ family = 'JetBrainsMono Nerd Font', weight = 'Bold', style = 'Normal', scale = SCALE }),
+			intensity = 'Bold',
+			italic = false,
+		},
+		{
+			font = font({ family = 'Maple Mono NF', scale = SCALE, style = 'Italic', weight = 'Regular' }),
+			intensity = 'Bold',
+			italic = true,
+		},
+		{
+			font = font({ family = 'Maple Mono NF', weight = 'Light', style = 'Italic', scale = SCALE }),
+			intensity = 'Half',
+			italic = true,
+		},
+		{
+			font = font({ family = 'Maple Mono NF', weight = 'Light', style = 'Italic', scale = SCALE }),
+			intensity = 'Normal',
+			italic = true,
+		},
+	}
+end
+
+return M
