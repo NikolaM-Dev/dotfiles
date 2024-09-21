151555aa617ea4ab70a5f31306e0c9560ac1c7fa
Juan David Merchan Torres <juan1014166@gmail.com>
2024-09-21 07:08:33 -0500

    feat(whatsapp): Add whatsapp-untrap.js script

---
+++ whatsapp/.config/whatsapp/whatsapp-untrap.js
@@ -0,0 +1,8 @@
+const statusElement = document.querySelector('[aria-label="Status"]');
+if (statusElement) statusElement.remove();
+
+const channelsElement = document.querySelector('[aria-label="Channels"]');
+if (channelsElement) channelsElement.remove();
+
+const communitiesElement = document.querySelector('[aria-label="Communities"]');
+if (communitiesElement) communitiesElement.remove();
