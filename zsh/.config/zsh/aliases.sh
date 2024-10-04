4479e35977b401cc413a914753eb95164808ba0d
Juan David Merchan Torres <juan1014166@gmail.com>
2024-10-04 08:14:20 -0500

    feat(zsh): Add configs aliases

---
+++ zsh/.config/zsh/aliases.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Configs
+alias A="cd ~/.config/alacritty && nvim alacritty.toml"
+alias C="cd ~/.config"
+alias csessions="gio trash ~/.local/share/nvim/sessions/"
+alias dbnvim="rm -rf ~/.local/share/nvim && rm -rf ~/.cache/nvim/lazy"
+alias dc="cd ~/Documents"
+alias D="cd ~/dotfiles && nvim"
+alias dl="cd ~/Downloads"
+alias g="cd ~/Documents/drive"
+alias G="cd ~/go/src/github.com/NikolaM-Dev"
+alias grub-cfg="sudo nvim /etc/default/grub"
+alias lb="cd ~/library"
+alias ncache="rm -rf ~/.cache/nvim/lazy"
+alias pacman-cfg="sudo nvim /etc/pacman.conf"
+alias P="cd ~/Pictures"
+alias R="cd ~/workspace/react"
+alias sb="cd ~/Documents/second-brain.md && nvim"
+alias ss="sudo -E -s"
+alias V="cd ~/.config/nvim && nvim"
+alias W="cd ~/workspace"
+alias year='cal -mwy'
+alias zc="nvim ~/.zshrc"
