84239134b3f8178d5b56065aa1fe5594a48dc897
Juan David Merchan Torres <juan1014166@gmail.com>
2024-11-12 07:38:18 -0500

    feat(yazi-plugins): Add smart-open plugin

---
+++ yazi/.config/yazi/plugins/smart-open.yazi/init.lua
@@ -0,0 +1,10 @@
+---@diagnostic disable: undefined-global
+
+--- @sync entry
+return {
+	entry = function()
+		local h = cx.active.current.hovered
+
+		ya.manager_emit(h and h.cha.is_dir and "enter" or "open", { hovered = true })
+	end,
+}
