45461836eb750d416205e13dc1419b814c62105d
Juan David Merchan Torres <juan1014166@gmail.com>
2024-11-18 10:30:24 -0500

    feat(yazi): Add smart-paste plugin

---
+++ yazi/.config/yazi/plugins/smart-paste.yazi/init.lua
@@ -0,0 +1,16 @@
+---@diagnostic disable: undefined-global
+
+--- @sync entry
+return {
+	entry = function()
+		local h = cx.active.current.hovered
+
+		if h and h.cha.is_dir then
+			ya.manager_emit("enter", {})
+			ya.manager_emit("paste", {})
+			ya.manager_emit("leave", {})
+		else
+			ya.manager_emit("paste", {})
+		end
+	end,
+}
