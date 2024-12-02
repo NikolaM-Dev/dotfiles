89bb8d11dcfbf36831941964599525071e7f2a9b
Juan David Merchan Torres <juan1014166@gmail.com>
2024-12-02 18:56:54 -0500

    style(surfingkeys): Change formatting

--- surfingkeys/.config/surfingkeys/settings.js
+++ surfingkeys/.config/surfingkeys/settings.js
@@ -1,15 +1,15 @@
 // an example to create a new mapping `ctrl-y`
-api.mapkey("<ctrl-y>", "Show me the money", function () {
+api.mapkey('<ctrl-y>', 'Show me the money', function () {
   Front.showPopup(
-    "a well-known phrase uttered by characters in the 1996 film Jerry Maguire (Escape to close).",
+    'a well-known phrase uttered by characters in the 1996 film Jerry Maguire (Escape to close).',
   );
 });
 
 // an example to replace `T` with `gt`, click `Default mappings` to see how `T` works.
-api.map("gt", "T");
+api.map('gt', 'T');
 
 // an example to remove mapkey `Ctrl-i`
-api.unmap("<ctrl-i>");
+api.unmap('<ctrl-i>');
 
 // set theme
 settings.theme = `
