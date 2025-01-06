da9e242e3b8e368009d675e4d4c38dba11ac78f0
Juan David Merchan Torres <juan1014166@gmail.com>
2025-01-05 19:58:06 -0500

    style(yazi): Change formatting

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -1,16 +1,14 @@
 ---@diagnostic disable: undefined-global
 Status:children_add(function()
 	local h = cx.active.current.hovered
-	if h == nil or ya.target_family() ~= "unix" then
+	if h == nil or ya.target_family() ~= 'unix' then
 		return ui.Line({})
 	end
 
 	return ui.Line({
-		ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg("yellow"),
-		ui.Span(":"),
-		ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg("yellow"),
-		ui.Span(" "),
-		ui.Span(ya.readable_size(h:size() or h.cha.length)):fg("green"),
-		ui.Span(" "),
+		ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg('yellow'),
+		ui.Span(':'),
+		ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg('yellow'),
+		ui.Span(' '),
 	})
 end, 500, Status.RIGHT)
