2d28548874ef1edd176f7914237d738ec6bb9604
Juan David Merchan Torres <juan1014166@gmail.com>
2025-01-24 08:41:59 -0500

    feat(zsh-keympas): Add _crowbar keymap

--- zsh/.config/zsh/keymaps.sh
+++ zsh/.config/zsh/keymaps.sh
@@ -9,4 +9,17 @@ function _open_with_nvim() {
 
 zle -N _open_with_nvim
 bindkey '^V' _open_with_nvim
+
 # ------------------------------------------------------------------------------
+# _crowbar, Toggle from a current process to the background
+# ------------------------------------------------------------------------------
+_crowbar() {
+  if [[ $#BUFFER == 0 ]]; then
+    fg >/dev/null 2>&1 && zle redisplay
+  else
+    zle push-input
+  fi
+}
+
+zle -N _crowbar
+bindkey '^Z' _crowbar
