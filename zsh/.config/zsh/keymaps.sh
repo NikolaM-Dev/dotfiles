b368ce036cd45e4413ab9ed5325601c775e44429
Juan David Merchan Torres <juan1014166@gmail.com>
2025-01-24 08:39:44 -0500

    docs(zsh-keympas): Add _open_with_nvim docs

--- zsh/.config/zsh/keymaps.sh
+++ zsh/.config/zsh/keymaps.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
+# ------------------------------------------------------------------------------
+# Open with nvim and launch telescope with smart open
+# ------------------------------------------------------------------------------
 function _open_with_nvim() {
   nvim -c ':Telescope smart_open theme=dropdown previewer=false'
 }
 
 zle -N _open_with_nvim
 bindkey '^V' _open_with_nvim
+# ------------------------------------------------------------------------------
