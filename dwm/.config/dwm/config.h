b6ce2a2659f6a481c128f4c4461fa03cbcb8b301
Juan David Merchan Torres <juan1014166@gmail.com>
2025-04-18 12:54:54 -0500

    fix(dwm): Fix compile errors about dmenu

--- dwm/.config/dwm/config.h
+++ dwm/.config/dwm/config.h
@@ -56,6 +56,8 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
+static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *emojis[]   = { "rofi", "-show", "emoji", NULL };
 static const char *menu[]     = { "rofi", "-show", NULL };
 static const char *term[]  = { "ghostty", "-e", "n-start-tmux", NULL };
