26859f43dc864c2394f86208c4e7ccefcaf74d92
Juan David Merchan Torres <juan1014166@gmail.com>
2025-04-21 13:06:39 -0500

    feat(dwm): Add rules to start obsidian & ticktick

--- dwm/.config/dwm/config.def.h
+++ dwm/.config/dwm/config.def.h
@@ -33,8 +33,8 @@ static const Rule rules[] = {
 	 */
 
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "obsidian", "obsidian",  NULL, 1 << 1, 0, -1 },
+	{ "ticktick", "ticktick",  NULL, 1 << 1, 0, -1 },
 };
 
 /* layout(s) */
