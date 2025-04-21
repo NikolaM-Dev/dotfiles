8075092af25244c2374587f8819bf0fa59985e43
Juan David Merchan Torres <juan1014166@gmail.com>
2025-04-21 13:06:00 -0500

    docs(dwm): Add utility to get rules properties

--- dwm/.config/dwm/config.def.h
+++ dwm/.config/dwm/config.def.h
@@ -25,7 +25,13 @@ static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
+
+	   	xprop | awk '
+	  		/^WM_CLASS/{sub(/.* =/, "instance:"); sub(/,/, "\nclass:"); print}
+          	        /^WM_NAME/{sub(/.* =/, "title:"); print}'
+
 	 */
+
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
