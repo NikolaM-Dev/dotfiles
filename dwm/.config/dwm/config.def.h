5279893624803453886ae21c83c7d4ff3a1014ed
Juan David Merchan Torres <juan1014166@gmail.com>
2025-04-22 19:22:34 -0500

    style(dwm): Change formatting

--- dwm/.config/dwm/config.def.h
+++ dwm/.config/dwm/config.def.h
@@ -1,20 +1,24 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx = 1;        /* border pixel of windows */
-static const unsigned int snap     = 32;       /* snap pixel */
-static const int horizpadbar       = 2;        /* horizontal padding for statusbar */
 static const int showbar           = 1;        /* 0 means no bar */
 static const int topbar            = 1;        /* 0 means bottom bar */
-static const char *fonts[]         = { "Maple Mono NF:size=10" };
-static const char dmenufont[]      = "Maple Mono NF:size=10";
-static const char col_gray1[]      = "#222222";
-static const char col_gray2[]      = "#444444";
-static const char col_gray3[]      = "#bbbbbb";
-static const char col_gray4[]      = "#eeeeee";
-static const char col_cyan[]       = "#005577";
-static const char *colors[][3]     = {
-static const int vertpadbar        = 8;        /* vertical padding for statusbar */
+static const unsigned int borderpx = 1;        /* border pixel of windows */
+static const unsigned int snap     = 32;       /* snap pixel */
+
+static const int horizpadbar = 2;        /* horizontal padding for statusbar */
+static const int vertpadbar  = 8;        /* vertical padding for statusbar */
+
+static const char *fonts[]    = { "JetBrains Mono:size=10:weight=medium", "Symbols Nerd Font Mono:size=10" };
+static const char dmenufont[] = "Maple Mono NF:size=10";
+
+static const char col_cyan[]   = "#005577";
+static const char col_gray1[]  = "#222222";
+static const char col_gray2[]  = "#444444";
+static const char col_gray3[]  = "#bbbbbb";
+static const char col_gray4[]  = "#eeeeee";
+
+static const char *colors[][3] = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { "#6e6a86", "#232136", "#2a283e" },
 	[SchemeSel]  = { "#e0def4", "#232136", "#56526e"  },
@@ -130,7 +134,7 @@ static const Key keys[] = {
 	{ MODKEY,             XK_r,                     spawn,      SHCMD("redshift -O 3500") },
 	{ MODKEY,             XK_e,                     spawn,      SHCMD("pcmanfm") },
 	{ MODKEY|ShiftMask,   XK_r,                     spawn,      SHCMD("redshift -x") },
-	{ MODKEY|ShiftMask,   XK_s,                     spawn,      SHCMD("scrot -d 3")},
+	{ MODKEY|ShiftMask,   XK_s,                     spawn,      SHCMD("scrot -d 3") },
 
 	// Volume
 	{ 0,                  XF86XK_AudioLowerVolume,  spawn,      SHCMD("pactl set-sink-volume @DEFAULT_SINK@ -2%") },
