4fa1fa3b568e329f85b721ad11fd6770287b4ebc
Juan David Merchan Torres <juan1014166@gmail.com>
2025-09-15 20:42:23 -0500

    docs(dmenu): Add docs about the dmenu highlight colors

--- dmenu/.config/dmenu/config.def.h
+++ dmenu/.config/dmenu/config.def.h
@@ -10,9 +10,9 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*      	 fg,        bg        */
-	[SchemeNorm] = { "#e0def4", "#232136" },
-	[SchemeSel]  = { "#2a283e", "#9ccfd8" },
-	[SchemeOut]  = { "#232136", "#9ccfd8" },
+	[SchemeNorm] = { "#e0def4", "#232136" }, /* SchemeNorm → default item colors. */
+	[SchemeOut]  = { "#232136", "#9ccfd8" }, /* SchemeOut → marked item colors (multi-selection mode). */
+	[SchemeSel]  = { "#2a283e", "#9ccfd8" }, /* SchemeSel → active/selected item colors. */
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
