ffbef502185b9b601ef1ec7d6f1a2ce3a132435f
Juan David Merchan Torres <juan1014166@gmail.com>
2025-09-15 20:39:42 -0500

    feat(dmenu): Change fonts
    Use JetBrains Mono as main with Symbols Nerd Font Mono as fallback

--- dmenu/.config/dmenu/config.def.h
+++ dmenu/.config/dmenu/config.def.h
@@ -4,6 +4,8 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
+	"JetBrains Mono:size=11:weight=medium",
+	"Symbols Nerd Font Mono:size=11",
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
