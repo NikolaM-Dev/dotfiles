a184312331c75af863aff8691c81fb8e932026b8
Juan David Merchan Torres <juan1014166@gmail.com>
2025-11-16 07:36:38 -0500

    feat(dmenu): Add dmenu-center patch options

--- dmenu/.config/dmenu/config.def.h
+++ dmenu/.config/dmenu/config.def.h
@@ -2,6 +2,9 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 1;                    /* -c option; centers dmenu on screen */
+static int min_width = 500;                    /* minimum width when centered */
+static const float menu_height_ratio = 2.0f;  /* This is the ratio used in the original calculation */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
   "Maple Mono NF:size=12",
