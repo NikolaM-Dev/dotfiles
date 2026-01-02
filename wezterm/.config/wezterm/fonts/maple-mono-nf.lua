621245953f30ad7a570eec54ebe7050b70dcfbb6
Juan David Merchan Torres <juan1014166@gmail.com>
2026-01-01 20:16:32 -0500

    feat(wezterm): Change maple-mono-nf config

--- wezterm/.config/wezterm/fonts/maple-mono-nf.lua
+++ wezterm/.config/wezterm/fonts/maple-mono-nf.lua
@@ -6,31 +6,32 @@ local M = {}
 
 function M.setup_font(config)
 	local SCALE = 0.98
+	local FAMILY = 'Maple Mono NF'
 
 	config.cell_width = 0.9
 	config.font_size = 13
 
-	config.font = font({ family = 'Maple Mono NF', scale = SCALE, style = 'Normal', weight = 'Light' })
+	config.font = font({ family = FAMILY, scale = SCALE, style = 'Normal' })
 
 	config.font_rules = {
 		{
-			font = font({ family = 'Maple Mono NF', weight = 'Bold', style = 'Normal', scale = SCALE }),
+			font = font({ family = FAMILY, scale = SCALE, style = 'Normal', weight = 'Bold' }),
 			intensity = 'Bold',
 			italic = false,
 		},
 		{
-			font = font({ family = 'Maple Mono NF', scale = SCALE, style = 'Italic', weight = 'Regular' }),
+			font = font({ family = FAMILY, scale = SCALE, style = 'Italic' }),
 			intensity = 'Bold',
 			italic = true,
 		},
 		{
-			font = font({ family = 'Maple Mono NF', weight = 'Light', style = 'Italic', scale = SCALE }),
-			intensity = 'Half',
+			font = font({ family = FAMILY, scale = SCALE, style = 'Italic' }),
+			intensity = 'Normal',
 			italic = true,
 		},
 		{
-			font = font({ family = 'Maple Mono NF', weight = 'Light', style = 'Italic', scale = SCALE }),
-			intensity = 'Normal',
+			font = font({ family = FAMILY, scale = SCALE, style = 'Italic' }),
+			intensity = 'Bold',
 			italic = true,
 		},
 	}
