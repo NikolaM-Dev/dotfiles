2033cf969e4b63982cbb3f26651f77951fa13aa9
Juan David Merchan Torres <juan1014166@gmail.com>
2026-01-22 09:44:53 -0500

    refactor(wezterm): Change to more lua idiomatic version

--- wezterm/.config/wezterm/config.lua
+++ wezterm/.config/wezterm/config.lua
@@ -1,14 +1,9 @@
 local wezterm = require('wezterm') --[[@as Wezterm]]
 
-local M = {}
 local config = {}
 
 if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
-function M.get_config()
-	return config
-end
-
-return M
+return config
